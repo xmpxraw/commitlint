bdee4b26eaa1d8c2c5c0706bc68745ba51c262a6
张幸明 <zhangxingming@corp.netease.com>
2022-03-24 16:03:31 +0800

    修改commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,54 @@
 module.exports = {
+  // extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: ['eslint:recommended'],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    sourceType: 'module'
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+    sourceType: 'module',
+    ecmaVersion: 6,
+  },
+  plugins: ['react', 'standard', '@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+      },
+      alias: [['@', './src']],
+    },
   },
   rules: {
-    // 'prettier/prettier': 'error',
-    // '@typescript-eslint/no-var-requires': 0,
-    // '@typescript-eslint/no-explicit-any': 0,
-    // '@typescript-eslint/no-empty-function': 0,
+    // semi: 0,
+    // indent: 0,
+    // 'react/jsx-filename-extension': 0,
+    // 'react/prop-types': 0,
+    // 'react/jsx-props-no-spreading': 0,
+
+    // 'jsx-a11y/click-events-have-key-events': 0,
+    // 'jsx-a11y/no-static-element-interactions': 0,
+    // 'jsx-a11y/no-noninteractive-element-interactions': 0,
+    // 'jsx-a11y/anchor-is-valid': 0,
+
+    // 'no-use-before-define': 0,
+    // 'no-unused-vars': 0,
+    // 'implicit-arrow-linebreak': 0,
+    // 'consistent-return': 0,
+    // 'arrow-parens': 0,
+    // 'object-curly-newline': 0,
+    // 'operator-linebreak': 0,
+    // 'import/no-extraneous-dependencies': 0,
+    // 'import/extensions': 0,
+    // 'import/no-unresolved': 0,
+    // 'import/prefer-default-export': 0,
+
     // '@typescript-eslint/ban-ts-comment': 0,
-    // '@typescript-eslint/no-this-alias': 0
+    // '@typescript-eslint/no-var-requires': 0,
   },
-  // plugins: ['prettier', '@typescript-eslint'],
-  // extends: ['prettier', 'plugin:@typescript-eslint/recommended']
 }
--- src/index.js
+++
@@ -1,3 +0,0 @@
-console.log(111)
-const a = 1111;
-console.log(a)
