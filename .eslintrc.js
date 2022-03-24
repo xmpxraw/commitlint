26d5b56c1a5d86623ab728d13f6a1dde557ecabd
张幸明 <zhangxingming@corp.netease.com>
2022-03-24 15:45:44 +0800

    text commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  rules: {
+    // 'prettier/prettier': 'error',
+    // '@typescript-eslint/no-var-requires': 0,
+    // '@typescript-eslint/no-explicit-any': 0,
+    // '@typescript-eslint/no-empty-function': 0,
+    // '@typescript-eslint/ban-ts-comment': 0,
+    // '@typescript-eslint/no-this-alias': 0
+  },
+  // plugins: ['prettier', '@typescript-eslint'],
+  // extends: ['prettier', 'plugin:@typescript-eslint/recommended']
+}
